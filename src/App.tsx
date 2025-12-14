09ac8535b12362ad419dc6cee67e9d4da93fee4a
Chris Ahn <chrisahn1007@gmail.com>
2025-12-14 15:54:56 +0900

    fix: 404

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
   return (
     <AuthProvider>
       <ScoreboardProvider>
-        <BrowserRouter>
+        <BrowserRouter basename="/REACT-FRONT">
           <Routes>
             <Route path="/" element={<ScoreboardListPage />} />
             <Route path="/login" element={<LoginPage />} />
